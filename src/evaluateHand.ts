abcb0e13eccb89c7b09905be9c9d9edc21e049fb
Henri Molloka <molloka@rushstreetinteractive.com>
2023-11-30 10:56:59 +0200

    v0.3 Detect pairs in hole cards

--- src/evaluateHand.ts
+++ src/evaluateHand.ts
@@ -1,18 +1,28 @@
 import {CommunityCard, HoleCard} from './interfaces';
 
 export enum HandStrength {
-  HIGH_CARD = 0,
-  PAIR = 1,
-  TWO_PAIRS = 2,
-  THREE_OF_A_KIND = 3,
-  STRAIGHT = 4,
-  FLUSH = 5,
-  FULL_HOUSE = 6,
-  FOUR_OR_A_KIND = 7,
-  STRAIGHT_FLUSH = 8,
-  ROYAL_FLUSH = 9,
+    HIGH_CARD = 0,
+    PAIR = 1,
+    TWO_PAIRS = 2,
+    THREE_OF_A_KIND = 3,
+    STRAIGHT = 4,
+    FLUSH = 5,
+    FULL_HOUSE = 6,
+    FOUR_OR_A_KIND = 7,
+    STRAIGHT_FLUSH = 8,
+    ROYAL_FLUSH = 9,
 }
 
 export default function evaluateHand(holeCards?: HoleCard[], communityCards?: CommunityCard[]): number {
-  return 0;
+
+    if (communityCards?.length == 0 && holeCards?.length == 2) {
+        var first = holeCards[0];
+        var second = holeCards[1];
+
+        if (first.rank == second.rank)
+            return HandStrength.PAIR;
+    }
+
+
+    return 0;
 }
